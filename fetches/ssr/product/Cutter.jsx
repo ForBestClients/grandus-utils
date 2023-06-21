29efce526d575602d6d222eb036156de7318648b
Andras Risnyovszky <risnyo96@gmail.com>
2023-06-21 09:13:02 +0200

    CUTTER - add fetch from elastic

--- fetches/ssr/product/Cutter.jsx
+++ fetches/ssr/product/Cutter.jsx
@@ -1,5 +1,11 @@
 import { reqApiHost, reqGetHeaders } from 'grandus-lib/utils';
-import { CUTTER_MATTE_PATH, CUTTER_SHINY_PATH } from 'utils/cutter';
+import {
+  CUTTER_MATTE_ELASTIC_PATH,
+  CUTTER_MATTE_PATH,
+  CUTTER_SHINY_ELASTIC_PATH,
+  CUTTER_SHINY_PATH,
+} from 'utils/cutter';
+import get from 'lodash/get';
 
 export const getCutterShinyPromise = () => {
   const req = {};
@@ -21,6 +27,28 @@ export const getCutterMattePromise = () => {
   })
     .then(result => result.json())
     .then(r => r.data);
+};
+
+export const getCutterShinyElasticPromise = () => {
+  const req = {};
+
+  return fetch(`${reqApiHost(req)}${CUTTER_SHINY_ELASTIC_PATH}`, {
+    headers: reqGetHeaders(req),
+    next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
+  })
+    .then(result => result.json())
+    .then(r => get(r, 'data[0]'));
+};
+
+export const getCutterMatteElasticPromise = () => {
+  const req = {};
+
+  return fetch(`${reqApiHost(req)}${CUTTER_MATTE_ELASTIC_PATH}`, {
+    headers: reqGetHeaders(req),
+    next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
+  })
+    .then(result => result.json())
+    .then(r => get(r, 'data[0]'));
 };
 
 const getCutterData = async () => {
