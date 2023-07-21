956d65446a24de2b5d5c311287079d345ccf88b0
Ivan Kopcik <ivan.kopcik@gmail.com>
2023-07-21 15:24:07 +0200

    removed unused methods

--- hooks/useFilter.js
+++ hooks/useFilter.js
@@ -15,51 +15,45 @@ import {
   flatten,
   indexOf,
   isObject,
-} from "lodash";
-import useSWR from "swr";
-import { RESERVED_URI_PARTS } from "grandus-lib/constants/UrlConstants";
-import { CATEGORY_PARAMETERS_SHOW_LIMIT } from "grandus-lib/constants/AppConstants";
-import { useRouter } from "next/navigation";
+} from 'lodash';
+import useSWR from 'swr';
+import { RESERVED_URI_PARTS } from 'grandus-lib/constants/UrlConstants';
+import { CATEGORY_PARAMETERS_SHOW_LIMIT } from 'grandus-lib/constants/AppConstants';
+import { useRouter } from 'next/navigation';
 
-const replaceKeyForUrlTitle = (key) =>
-  get(find(RESERVED_URI_PARTS, ["key", key]), "urlTitle", key);
-
-const replaceUrlTitleForKey = (urlTitle) =>
-  get(find(RESERVED_URI_PARTS, ["urlTitle", urlTitle]), "key", urlTitle);
-
-const sortChunks = (chunks) => chunks; // temporary disabled sortingsortBy(chunks, (pair) => pair[0]);
+const sortChunks = chunks => chunks; // temporary disabled sortingsortBy(chunks, (pair) => pair[0]);
 
 export const getSeoTitleData = (filter = {}) => {
   const titleData = [];
 
-  map(get(filter, "selected.stores.data", []), (store) => {
-    if (get(store, "name")) {
-      titleData.push(get(store, "name"));
+  map(get(filter, 'selected.stores.data', []), store => {
+    if (get(store, 'name')) {
+      titleData.push(get(store, 'name'));
     }
   });
 
-  map(get(filter, "selected.brands.data", []), (brand) => {
-    if (get(brand, "name")) {
-      titleData.push(get(brand, "name"));
+  map(get(filter, 'selected.brands.data', []), brand => {
+    if (get(brand, 'name')) {
+      titleData.push(get(brand, 'name'));
     }
   });
 
-  map(get(filter, "selected.storeLocations.data", []), (store) => {
-    if (get(store, "name")) {
-      titleData.push(get(store, "name"));
+  map(get(filter, 'selected.storeLocations.data', []), store => {
+    if (get(store, 'name')) {
+      titleData.push(get(store, 'name'));
     }
   });
 
-  map(get(filter, "selected.statuses.data", []), (status) => {
-    if (get(status, "name")) {
-      titleData.push(get(status, "name"));
+  map(get(filter, 'selected.statuses.data', []), status => {
+    if (get(status, 'name')) {
+      titleData.push(get(status, 'name'));
     }
   });
 
-  map(get(filter, "selected.parameters.data", []), (parameter) => {
-    map(get(parameter, "values", []), (value) => {
-      if (get(value, "value")) {
-        titleData.push(get(value, "value"));
+  map(get(filter, 'selected.parameters.data', []), parameter => {
+    map(get(parameter, 'values', []), value => {
+      if (get(value, 'value')) {
+        titleData.push(get(value, 'value'));
       }
     });
   });
@@ -68,7 +62,7 @@ export const getSeoTitleData = (filter = {}) => {
 };
 
 export const hasActiveFilters = (filter = {}) => {
-  return !isEmpty(omit(get(filter, "selected", []), "category"));
+  return !isEmpty(omit(get(filter, 'selected', []), 'category'));
 };
 
 export const getApiBodyFromParams = (params = []) => {
@@ -79,11 +73,11 @@ export const getApiBodyFromParams = (params = []) => {
   const newParams = { ...params };
   const apiBody = {};
 
-  map(RESERVED_URI_PARTS, (reserved) => {
+  map(RESERVED_URI_PARTS, reserved => {
     const identifiedReserved = get(
       newParams,
       [reserved.urlTitle],
-      get(newParams, [reserved.key])
+      get(newParams, [reserved.key]),
     );
     if (identifiedReserved) {
       if (isArray(reserved.key)) {
@@ -94,7 +88,7 @@ export const getApiBodyFromParams = (params = []) => {
         delete newParams[reserved.urlTitle];
       } else {
         apiBody[reserved.key] = isArray(identifiedReserved)
-          ? map(identifiedReserved, (ir) => decodeURIComponent(ir))
+          ? map(identifiedReserved, ir => decodeURIComponent(ir))
           : [decodeURIComponent(identifiedReserved)];
         delete newParams[reserved.urlTitle];
         delete newParams[reserved.key];
@@ -107,7 +101,7 @@ export const getApiBodyFromParams = (params = []) => {
   map(newParams, (item, key) => {
     if (item) {
       apiBody.param[key] = isArray(item)
-        ? map(item, (i) => decodeURIComponent(i))
+        ? map(item, i => decodeURIComponent(i))
         : [decodeURIComponent(item)];
     }
   });
@@ -115,14 +109,14 @@ export const getApiBodyFromParams = (params = []) => {
   return apiBody;
 };
 
-export const getApiBodyFromPath = (path) => {
+export const getApiBodyFromPath = path => {
   return getApiBodyFromParams(pathToParams(path));
 };
 
 export const queryToQuery = (
   query,
   dataToChange = {},
-  toDelete = ["parameters", "category"]
+  toDelete = ['parameters', 'category'],
 ) => {
   let newQuery = {
     ...query,
@@ -130,7 +124,7 @@ export const queryToQuery = (
   };
 
   if (!isEmpty(toDelete)) {
-    toDelete.map((key) => delete newQuery[key]);
+    toDelete.map(key => delete newQuery[key]);
   }
 
   return newQuery;
@@ -145,45 +139,45 @@ export const queryToQuery = (
 export const queryToQueryString = (
   query,
   dataToChange = {},
-  toDelete = ["parameters", "category"],
-  options = {}
+  toDelete = ['parameters', 'category'],
+  options = {},
 ) => {
   const queryAdjusted = queryToQuery(
     query,
     isObject(dataToChange) ? dataToChange : {},
-    isArray(toDelete) ? toDelete : []
+    isArray(toDelete) ? toDelete : [],
   );
 
   let queryParts = [];
 
   map(queryAdjusted, (value, key) => {
     queryParts.push(
-      `${get(options, ["replace", key], key)}=${
-        get(options, "encode") ? encodeURIComponent(value) : value
-      }`
+      `${get(options, ['replace', key], key)}=${
+        get(options, 'encode') ? encodeURIComponent(value) : value
+      }`,
     );
   });
 
-  return queryParts.join("&");
+  return queryParts.join('&');
 };
 
 export const getCategoryLinkAttributesFromRouter = (router, options = {}) => {
   return getCategoryLinkAttributes(
-    get(router, "query.category"),
-    arrayToPath(get(router, "query.parameters", [])),
+    get(router, 'query.category'),
+    arrayToPath(get(router, 'query.parameters', [])),
     router.query,
-    options
+    options,
   );
 };
 
 //tested
 export const getCategoryLinkAttributes = (
   category,
-  parameters = "",
+  parameters = '',
   query = {},
-  options = {}
+  options = {},
 ) => {
-  if (get(options, "absoluteHref")) {
+  if (get(options, 'absoluteHref')) {
     return { href: options.absoluteHref };
   }
 
@@ -202,13 +196,13 @@ export const getCategoryLinkAttributes = (
     return emptyResult;
   }
 
-  const newQuery = get(options, "toDelete")
+  const newQuery = get(options, 'toDelete')
     ? queryToQuery(
         query,
-        get(options, "dataToChange", {}),
-        get(options, "toDelete")
+        get(options, 'dataToChange', {}),
+        get(options, 'toDelete'),
       )
-    : queryToQuery(query, get(options, "dataToChange", {}));
+    : queryToQuery(query, get(options, 'dataToChange', {}));
   return {
     href: {
       pathname: `/kategoria/[category]/[[...parameters]]`,
@@ -223,35 +217,35 @@ export const getCategoryLinkAttributes = (
 
 export const getCampaignLinkAttributesFromRouter = (router, options = {}) => {
   return getCampaignLinkAttributes(
-    get(router, "query.campaign"),
-    arrayToPath(get(router, "query.parameters", [])),
+    get(router, 'query.campaign'),
+    arrayToPath(get(router, 'query.parameters', [])),
     router.query,
-    options
+    options,
   );
 };
 
 export const getSearchLinkAttributesFromRouter = (router, options = {}) => {
   return getSearchLinkAttributes(
-    encodeURIComponent(get(router, "query.term")),
-    arrayToPath(get(router, "query.parameters", [])),
+    encodeURIComponent(get(router, 'query.term')),
+    arrayToPath(get(router, 'query.parameters', [])),
     router.query,
-    options
+    options,
   );
 };
 
 export const getCampaignLinkAttributes = (
   campaign,
-  parameters = "",
+  parameters = '',
   query = {},
-  options = {}
+  options = {},
 ) => {
-  const newQuery = get(options, "toDelete")
+  const newQuery = get(options, 'toDelete')
     ? queryToQuery(
         query,
-        get(options, "dataToChange", {}),
-        get(options, "toDelete")
+        get(options, 'dataToChange', {}),
+        get(options, 'toDelete'),
       )
-    : queryToQuery(query, get(options, "dataToChange", {}));
+    : queryToQuery(query, get(options, 'dataToChange', {}));
   return {
     href: {
       pathname: `/akcie/[campaign]/[[...parameters]]`,
@@ -268,23 +262,23 @@ export const getSystemFilterAttributes = (data, key, options = {}) => {
   return {
     parameter: {
       id: key,
-      name: get(options, "name")
-        ? get(options, "name")
-        : get(find(RESERVED_URI_PARTS, ["key", key]), "title", key),
-      urlTitle: get(find(RESERVED_URI_PARTS, ["key", key]), "urlTitle", key),
+      name: get(options, 'name')
+        ? get(options, 'name')
+        : get(find(RESERVED_URI_PARTS, ['key', key]), 'title', key),
+      urlTitle: get(find(RESERVED_URI_PARTS, ['key', key]), 'urlTitle', key),
       values: data,
     },
-    handleChange: get(options, "handleChange"),
-    selected: get(options, "selected"),
+    handleChange: get(options, 'handleChange'),
+    selected: get(options, 'selected'),
     options: {
-      styles: get(options, "styles", {}),
+      styles: get(options, 'styles', {}),
       ...getShowMoreAttributes(
         {
           id: key,
           values: data,
         },
-        get(options, "openedParameter"),
-        get(options, "onClickToggleOpen")
+        get(options, 'openedParameter'),
+        get(options, 'onClickToggleOpen'),
       ),
       ...options,
     },
@@ -295,17 +289,17 @@ export const getShowMoreAttributes = (
   parameter,
   opened,
   onClickToggleOpen,
-  options = {}
+  options = {},
 ) => {
   return {
     showMoreEnabled:
-      get(parameter, "values", []).length >
-      get(options, "parametersShowLimit", CATEGORY_PARAMETERS_SHOW_LIMIT),
-    showMoreActive: !(indexOf(opened, get(parameter, "id")) >= 0),
+      get(parameter, 'values', []).length >
+      get(options, 'parametersShowLimit', CATEGORY_PARAMETERS_SHOW_LIMIT),
+    showMoreActive: !(indexOf(opened, get(parameter, 'id')) >= 0),
     showMoreLimit: get(
       options,
-      "parametersShowLimit",
-      CATEGORY_PARAMETERS_SHOW_LIMIT
+      'parametersShowLimit',
+      CATEGORY_PARAMETERS_SHOW_LIMIT,
     ),
     showMoreToggle: onClickToggleOpen,
   };
@@ -313,17 +307,17 @@ export const getShowMoreAttributes = (
 
 export const getSearchLinkAttributes = (
   searchTerm,
-  parameters = "",
+  parameters = '',
   query = {},
-  options = {}
+  options = {},
 ) => {
-  const newQuery = get(options, "toDelete")
+  const newQuery = get(options, 'toDelete')
     ? queryToQuery(
         query,
-        get(options, "dataToChange", {}),
-        get(options, "toDelete")
+        get(options, 'dataToChange', {}),
+        get(options, 'toDelete'),
       )
-    : queryToQuery(query, get(options, "dataToChange", {}));
+    : queryToQuery(query, get(options, 'dataToChange', {}));
   return {
     href: {
       pathname: `/vyhladavanie/[term]/[[...parameters]]`,
@@ -336,37 +330,37 @@ export const getSearchLinkAttributes = (
   };
 };
 
-export const arrayToParams = (array) => {
+export const arrayToParams = array => {
   if (isEmpty(array)) {
     return {};
   }
   return fromPairs(
     sortChunks(
       chunk(
-        map(array, (value) =>
-          map(split(value, ","), (val) => encodeURIComponent(val))
+        map(array, value =>
+          map(split(value, ','), val => encodeURIComponent(val)),
         ),
-        2
-      )
-    )
+        2,
+      ),
+    ),
   );
 };
 
-export const pathToParams = (path) => {
+export const pathToParams = path => {
   if (!path) {
     return {};
   }
-  return arrayToParams(split(path, "/"));
+  return arrayToParams(split(path, '/'));
 };
 
-export const arrayToPath = (array) => {
+export const arrayToPath = array => {
   if (isEmpty(array)) {
-    return "";
+    return '';
   }
-  return flatten(sortChunks(chunk(array, 2))).join("/");
+  return flatten(sortChunks(chunk(array, 2))).join('/');
 };
 
-export const paramsToPath = (params) => {
+export const paramsToPath = params => {
   return arrayToPath(flatMap(params, (value, key) => [key, value]));
 };
 
@@ -383,15 +377,15 @@ const useFilter = ({
   let uri = [];
 
   if (useDataFromRouter) {
-    map(get(router, "query"), (uriPart, index) => {
+    map(get(router, 'query'), (uriPart, index) => {
       switch (index) {
-        case "category":
+        case 'category':
           uri.push(`id=${uriPart}`);
           break;
-        case "term":
+        case 'term':
           uri.push(`search=${uriPart}`);
           break;
-        case "parameters":
+        case 'parameters':
           uri.push(`param=${arrayToPath(uriPart)}`);
           break;
 
@@ -421,13 +415,13 @@ const useFilter = ({
   }
 
   const filterRoute = allProducts ? 'filters/all' : 'filters';
-  const url = `/api/lib/v1/${filterRoute}?${uri.join("&")}`;
+  const url = `/api/lib/v1/${filterRoute}?${uri.join('&')}`;
 
   const {
     data: filter,
     mutate,
     isValidating,
-  } = useSWR(url, (url) => fetch(url).then((r) => r.json()), {
+  } = useSWR(url, url => fetch(url).then(r => r.json()), {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
     shouldRetryOnError: true,
