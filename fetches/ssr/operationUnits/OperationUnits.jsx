5156ab5b15f9f2b5a22e5a034dd0a777e0b28d56
Patrik Spilka <patrik@forbestclients.com>
2023-08-16 12:13:02 +0200

    OperationUnits - expand fix

--- fetches/ssr/operationUnits/OperationUnits.jsx
+++ fetches/ssr/operationUnits/OperationUnits.jsx
@@ -3,18 +3,15 @@ import { reqApiHost, reqGetHeaders } from '/grandus-lib/utils';
 async function getOperationUnits() {
   const req = {};
 
+  const expand = process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
+    ? process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
+    : 'openingHours, town.county, parameters';
+
   const [operationUnits] = await Promise.all([
-    fetch(
-      `${reqApiHost(
-        req,
-      )}/api/v2/operation-units?expand=${
-        process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
-      }`,
-      {
-        headers: reqGetHeaders(req),
-        next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
-      },
-    )
+    fetch(`${reqApiHost(req)}/api/v2/operation-units?expand=${expand}`, {
+      headers: reqGetHeaders(req),
+      next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
+    })
       .then(result => result.json())
       .then(r => r.data),
   ]);
