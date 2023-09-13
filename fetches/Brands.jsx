e836c6b4addc414e586ff0d804a68a7c09aef29c
Ivan Kopcik <ivan.kopcik@gmail.com>
2023-09-13 15:22:49 +0200

    added revalidate to Brand fetch

--- fetches/Brands.jsx
+++ fetches/Brands.jsx
@@ -1,17 +1,19 @@
 import { reqGetHeaders, reqApiHost } from 'grandus-lib/utils';
 
-const getBrands = async (props) => {
+const getBrands = async props => {
   const req = {};
 
-
   const data = await fetch(
-    `${reqApiHost(req)}/api/v2/brands?per-page=${props?.perPage ? props?.perPage : "12"}&orderBy=priority-desc`,
+    `${reqApiHost(req)}/api/v2/brands?per-page=${
+      props?.perPage ? props?.perPage : '12'
+    }&orderBy=priority-desc`,
     {
       headers: reqGetHeaders(req),
-    }
+      next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
+    },
   )
-    .then((result) => result.json())
-    .then((r) => r.data);
+    .then(result => result.json())
+    .then(r => r.data);
 
   return data;
 };
