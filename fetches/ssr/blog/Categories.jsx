42dfb9269b3e76efed6c95643f330a851ca60ca5
rasevp <pavol@forbestclients.com>
2024-07-31 13:05:15 +0200

    BLOG CATEGORIES - add forum categories filtering

--- fetches/ssr/blog/Categories.jsx
+++ fetches/ssr/blog/Categories.jsx
@@ -1,10 +1,20 @@
 import { reqApiHost, reqGetHeaders } from 'grandus-lib/utils';
+import isEmpty from "lodash/isEmpty";
 
-async function getCategoriesData() {
+async function getCategoriesData(props) {
   const req = {};
 
-  const [categories] = await Promise.all([
-    fetch(`${reqApiHost(req)}/api/v2/blogs/categories`, {
+  const uri = [];
+  if (props?.forum !== undefined) {
+    uri.push(`forum=${props?.forum}`)
+  }
+
+  const url = `${reqApiHost(req)}/api/v2/blogs/categories${
+    !isEmpty(uri) ? `?${uri.join('&')}` : ''
+  }`;
+
+  return await
+    fetch(url, {
       headers: reqGetHeaders(req),
       next: {
         revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE),
@@ -12,9 +22,6 @@ async function getCategoriesData() {
       },
     })
       .then(result => result.json())
-      .then(r => r.data),
-  ]);
-
-  return categories;
+      .then(r => r.data);
 }
 export default getCategoriesData;
