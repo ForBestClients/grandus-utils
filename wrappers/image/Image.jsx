90193c2bcfdc3d456b67a7d0e1b42aec9067c80c
rasevp <pavol@forbestclients.com>
2024-08-13 13:19:54 +0200

    TEST - retina images performance

--- wrappers/image/Image.jsx
+++ wrappers/image/Image.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { getImageUrl } from 'grandus-lib/utils/index';
+import {getImageUrl, isRetina} from 'grandus-lib/utils/index';
 
 const ImageWrapped = ({
   photo,
@@ -11,8 +11,8 @@ const ImageWrapped = ({
   alt,
   priority,
   className,
-  quality = 75,
-  suffix = "",
+  quality = 80,
+  suffix = "@2x",
 }) => {
   const src = getImageUrl(photo, `${width}x${height}${suffix ? suffix : ''}`, type);
 
