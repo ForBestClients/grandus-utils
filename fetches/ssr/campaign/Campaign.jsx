5b9751a40b2124c99b79778ce7da5f2738736c77
rasevp <pavol@forbestclients.com>
2024-11-05 19:31:31 +0100

    TESTING - fixing campaigns

--- fetches/ssr/campaign/Campaign.jsx
+++ fetches/ssr/campaign/Campaign.jsx
@@ -1,4 +1,4 @@
-import { reqApiHost, reqGetHeaders, getPaginationFromHeaders } from 'grandus-lib/utils';
+import { reqGetHeaders } from 'grandus-lib/utils';
 import get from 'lodash/get';
 import {getApiExpand, reqGetHost} from "@/grandus-utils";
 import {headers} from "next/headers";
@@ -15,19 +15,14 @@ const getCampaign = async data => {
         uri.push(getApiExpand('CAMPAIGN_DETAIL', true));
     }
 
-    try {
-        return await fetch(
-            `${reqGetHost(req)}/api/lib/v1/campaigns/${params?.urlTitle}?${uri.join("&")}`,
-            {
-                headers: reqGetHeaders(req),
-                next: {revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE)},
-            }
-        )
-            .then((result) => result.json());
-    } catch (err) {
-        console.error(err.message);
-        return {};
-    }
+    return await fetch(
+        `${reqGetHost(req)}/api/lib/v1/campaigns/${params?.urlTitle}?${uri.join("&")}`,
+        {
+            headers: reqGetHeaders(req),
+            next: {revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE)},
+        }
+    )
+        .then((result) => result.json());
 
 };
 
