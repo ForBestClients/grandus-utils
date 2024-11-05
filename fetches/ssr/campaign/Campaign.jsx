5964d5278958cbfc9d5ecec7552f422136022427
rasevp <pavol@forbestclients.com>
2024-11-05 18:07:16 +0100

    TESTING - fixing campaigns

--- fetches/ssr/campaign/Campaign.jsx
+++ fetches/ssr/campaign/Campaign.jsx
@@ -1,52 +1,33 @@
-import {reqApiHost, reqGetHeaders, getFrontendUrlFromHeaders} from 'grandus-lib/utils';
+import { reqApiHost, reqGetHeaders, getPaginationFromHeaders } from 'grandus-lib/utils';
 import get from 'lodash/get';
-import {getApiExpand, reqGetHost, } from "@/grandus-utils";
-import getCampaigns from "@/grandus-utils/fetches/ssr/campaign/Campaigns";
-import {queryToQueryString} from "@/grandus-lib/utils/filter";
+import {getApiExpand, reqGetHost} from "@/grandus-utils";
+import {headers} from "next/headers";
 
 const getCampaign = async data => {
-    const req = {};
-    const query = {};
+    const req = { headers: { host: headers().get('host') } };
+    const uri = [];
 
     const params = get(data, 'params');
 
-    query.expand = params?.expand ? params?.expand : getApiExpand('CAMPAIGN_DETAIL', true)
+    if (params?.expand) {
+        uri.push(`expand=${params?.expand}`);
+    } else {
+        uri.push(getApiExpand('CAMPAIGN_DETAIL', true));
+    }
 
-    const urlCampaign = params?.urlTitle;
-    const urlRest = queryToQueryString(query, {}, ["campaign"], {
-        encode: true,
-    });
-
-    let [campaigns, campaign, products, filter] = await Promise.all([
-        getCampaigns(),
-        fetch(
-            `${reqApiHost(req)}/api/v2/campaigns/${urlCampaign}?expand=coverPhoto`,
+    try {
+        return await fetch(
+            `${reqGetHost(req)}/api/lib/v1/campaigns/${params?.urlTitle}?${uri.join("&")}`,
             {
                 headers: reqGetHeaders(req),
+                next: {revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE)},
             }
-        ).then((result) => result.json()),
-        fetch(
-            `${reqGetHost(req)}/api/lib/v1/products?marketingCampaign=${urlCampaign}&${urlRest}`
-        ).then((result) => result.json()),
-        fetch(
-            `${reqGetHost(req)}/api/lib/v1/filters?marketingCampaign=${urlCampaign}&${urlRest}`,
-            {
-                headers: reqGetHeaders(req, {
-                    forwardUrl: getFrontendUrlFromHeaders(req?.headers),
-                }),
-            }
-        ).then((result) => result.json()),
-    ]);
-
-    return {
-        campaigns: get(campaigns, "campaigns", {}),
-        campaign: get(campaign, "data", {}),
-        products: get(products, "products", []),
-        pagination: get(products, "pagination", {}),
-        filter: filter,
-        meta: get(filter, "meta", {}),
-        breadcrumbs: get(filter, "breadcrumbs", {}),
-    };
+        )
+            .then((result) => result.json());
+    } catch (err) {
+        console.error(err.message);
+        return {};
+    }
 
 };
 
