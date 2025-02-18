05317b945765275b290b8411d3ea6fc430889037
Ivan Kopcik <ivan.kopcik@gmail.com>
2025-02-18 12:04:43 +0100

    added options for filtering to api

--- fetches/ssr/operationUnits/OperationUnits.jsx
+++ fetches/ssr/operationUnits/OperationUnits.jsx
@@ -1,14 +1,29 @@
 import { reqApiHost, reqGetHeaders } from '/grandus-lib/utils';
 
-async function getOperationUnits() {
+async function getOperationUnits(params) {
   const req = {};
+  const uri = [];
 
-  const expand = process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
-    ? process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
-    : 'openingHours, town.county, parameters';
+  if (params?.expand) {
+    uri.push(`expand=${params?.expand}`);
+  } else {
+    uri.push(
+      `expand=${
+        process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
+          ? process.env.NEXT_PUBLIC_OPERATION_UNITS_EXPAND
+          : 'openingHours, town.county, parameters'
+      }`,
+    );
+  }
+
+  if (params?.parentId) {
+    uri.push(`parentId=${params?.parentId}`);
+  }
+
+  const uriString = uri?.length ? `?${uri.join('&')}` : '';
 
   const [operationUnits] = await Promise.all([
-    fetch(`${reqApiHost(req)}/api/v2/operation-units?expand=${expand}`, {
+    fetch(`${reqApiHost(req)}/api/v2/operation-units${uriString}`, {
       headers: reqGetHeaders(req),
       next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
     })
