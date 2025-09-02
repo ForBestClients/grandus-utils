0809d812281a8a96bec7a25bca14ed58a6e8442a
Pavol Rasev <pavol.rasev@gmail.com>
2025-09-02 10:48:10 +0200

    LIBS - prefix with cz

--- fetches/ssr/campaign/Campaign.jsx
+++ fetches/ssr/campaign/Campaign.jsx
@@ -16,7 +16,7 @@ const getCampaign = async data => {
     }
 
     return await fetch(
-        `${reqGetHost(req)}/api/lib/v1/campaigns/${params?.urlTitle}?${uri.join("&")}`,
+        `${reqGetHost(req)}/cz/api/lib/v1/campaigns/${params?.urlTitle}?${uri.join("&")}`,
         {
             headers: reqGetHeaders(req),
             next: {revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE)},
