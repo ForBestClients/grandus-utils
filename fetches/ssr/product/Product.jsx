5de4670bbb2697755248b14b00dedea8b8812853
András Risnyovszký <andras@forbestclients.com>
2025-09-23 09:15:06 +0200

    PRODUCT - revalidate options

--- fetches/ssr/product/Product.jsx
+++ fetches/ssr/product/Product.jsx
@@ -2,7 +2,7 @@ import { reqApiHost, reqGetHeadersBasic } from 'grandus-lib/utils/edge';
 import { getApiExpand, getApiFields } from 'grandus-utils';
 import { getProcessedCardFields } from 'utils';
 
-export const getProductPromise = async params => {
+export const getProductPromise = async (params, options = {}) => {
   const req = {};
 
   const uri = [];
@@ -22,17 +22,30 @@ export const getProductPromise = async params => {
     uri.push(getApiExpand('PRODUCT_DETAIL', true));
   }
 
+  const revalidate = options?.revalidate;
+
+  const reqOptions = {
+    headers: reqGetHeadersBasic(req),
+  };
+
+  if (typeof revalidate === 'number') {
+    reqOptions.next = { revalidate: revalidate };
+  } else if (revalidate === null) {
+    reqOptions.cache = 'no-store';
+  } else {
+    reqOptions.next = {
+      revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE),
+    };
+  }
+
   return fetch(
     `${reqApiHost(req)}/api/v2/products/${params?.urlTitle}?${uri.join('&')}`,
-    {
-      headers: reqGetHeadersBasic(req),
-      next: { revalidate: Number(process.env.NEXT_PUBLIC_REVALIDATE) },
-    },
+    reqOptions,
   ).then(result => result.json());
 };
 
-export const getProductData = async params => {
-  const [product] = await Promise.all([getProductPromise(params)]);
+export const getProductData = async (params, options = {}) => {
+  const [product] = await Promise.all([getProductPromise(params, options)]);
 
   return product;
 };
